af0cb6a91f7bbd17adc8d9f100d2f9ab6acf06cb
Kamil <kamilbykowski99@gmail.com>
2025-04-24 14:47:07 +0200

    fix isLoading in TodoModal

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -11,13 +11,13 @@ type Props = {
 
 export const TodoModal: React.FC<Props> = ({ todo, setIsModalShowed }) => {
   const [user, setUser] = useState<User | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (todo) {
       getUser(todo.userId)
         .then(json => setUser(json))
-        .finally(() => setIsLoading(true));
+        .finally(() => setIsLoading(false));
     }
   }, []);
 
@@ -30,7 +30,7 @@ export const TodoModal: React.FC<Props> = ({ todo, setIsModalShowed }) => {
     <div className="modal is-active" data-cy="modal">
       <div className="modal-background" />
 
-      {!isLoading ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <div className="modal-card">
